e0c348830d02e9b53999e0b6d08b376370a8bc2b
Yarous <slavluch10@gmail.com>
2024-03-05 21:39:28 +0300

    finalized API, it will stay after final version of Crunch

--- CrunchAPI/module.py
+++ CrunchAPI/module.py
@@ -1,10 +1,10 @@
 import socket, json
-def fetch_and_parse_json(host, path="/", port=80):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        s.sendall(f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n".encode())
-        response = b"".join(iter(lambda: s.recv(4096), b''))
-    try: return json.loads(response.decode('utf-8').split('\r\n\r\n', 1)[1])
-    except json.JSONDecodeError: return None
 
-print(fetch_and_parse_json('127.0.0.1', '/api/tests'))
+def getTest(id):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(('127.0.0.1', 80))
+            s.sendall(f"GET /api/test/{id} HTTP/1.1\r\nHost: 127.0.0.1\r\nConnection: close\r\n\r\n".encode())
+            response = b"".join(iter(lambda: s.recv(4096), b''))
+        return json.loads(response.decode('utf-8').split('\r\n\r\n', 1)[1])
+    except json.JSONDecodeError:  return None
