80e3ab9f1b0cf5a96f181b0fdc54fe5c77bd2b49
Yarous <slavluch10@gmail.com>
2024-03-10 01:31:51 +0300

    example for the Test class

--- CrunchAPI/module.py
+++ CrunchAPI/module.py
@@ -19,8 +19,7 @@ class Test:
 
     def addTest(self, testInfo):
         if not isinstance(testInfo, dict): return ValueError("The argument passed is not a dict")
-
-        # Обновленная проверка структурной соответствия новому образу JSON
+        
         required_keys = ["testName", "testDescription", "question", "options", "selectedOption"]
         if not all(key in testInfo for key in required_keys): return ValueError("JSON structure does not match the expected format")
 
@@ -33,11 +32,3 @@ class Test:
                 s.sendall(f"POST /api/addtest HTTP/1.1\r\nHost: {self.host}\r\nContent-Type: application/json\r\nContent-Length: {len(data_string)}\r\n\r\n{data_string}".encode())
                 return b"".join(iter(lambda: s.recv(4096), b'')).decode().split('\r\n\r\n', 1)[1]
         except Exception as e: return str(e)
-
-
-
-# Пример использования
-li = {'testName': 'name1111', 'testDescription': 'descr2', 'question': 'qst', 'options': ['pick 1', 'pick 2', 'pick 3', 'pick 4', 'pick 5'], 'selectedOption': 3}
-
-test = Test()
-print(test.addTest(li))
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-li = {"kyh": 1}
-print(type(li))
+from CrunchAPI import Test
+
+test = Test()
+
+info = {'testName': 'name1111', 'testDescription': 'descr2', 'question': 'qst', 'options': ['pick 1', 'pick 2', 'pick 3', 'pick 4', 'pick 5'], 'selectedOption': 3}
+print(test.addTest(info))
